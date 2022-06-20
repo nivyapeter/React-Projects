8effca36be990fcebcb692a99873265c5a5a995f
nivyapeter <nivyakpeter@gmail.com>
2022-06-20 11:33:06 +0530

    header section in ktdc

---
+++ ktdc-site/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div className="border-t-2 border-[#f1a93a] bg-[#3d3d3d] px-[15px]">
+        <div className="max-w-[1360px] m-auto px-[15px] w-full">
+          <ul className="my-0 mx-auto ">
+            <li>
+              <a
+                className="text-white text-[13px] py-[10px] px-[15px] uppercase"
+                href=""
+              ></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
